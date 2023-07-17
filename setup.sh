319b756edfefa6e7b54ce670d27185755de6b417
christinaliu <cliu7@caltech.edu>
2023-07-17 12:22:42 -0700

    change setup

--- setup.sh
+++ setup.sh
@@ -14,13 +14,18 @@ pip3 install git+https://github.com/huggingface/diffusers.git
 
 pip3 install -r requirements.txt
 
-wget https://zenodo.org/record/7964925/files/checkpoints.zip?download=1
+#wget https://zenodo.org/record/7964925/files/checkpoints.zip?download=1
 
-#wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 
-#python dataset_tool.py --source=cifar-10-python.tar.gz --dest=datasets/cifar10-32x32.zip
-#
-#unzip cifar10-32x32.zip
+python dataset_tool.py --source=cifar-10-python.tar.gz --dest=datasets/cifar10-32x32.zip
+
+unzip cifar10-32x32.zip
 
 
-#torchrun --standalone --nproc_per_node=1 train.py --outdir=result --experiment_name=ambient-diffusion --dump=200 --cond=0 --arch=ddpmpp --precond=ambient --cres=1,2,2,2 --lr=2e-4 --dropout=0.1 --augment=0.15 --data=datasets --norm=2 --max_grad_norm=1.0 --mask_full_rgb=True --corruption_probability=0.4 --delta_probability=0.1 --batch=256 --max_size=30000
+#torchrun --standalone --nproc_per_node=1 train.py --outdir=result --experiment_name=ambient-diffusion --dump=200 --cond=0 --arch=ddpmpp --precond=ambient --cres=1,2,2,2 --lr=2e-4 --dropout=0.1 --augment=0.15 --data=datasets --norm=2 --max_grad_norm=1.0 --mask_full_rgb=True --corruption_probability=0.4 --delta_probability=0.1 --batch=32 --max_size=300
+
+
+#EXPERIMENT_NAME=ambient-diffusion
+#GPUS_PER_NODE=1
+#torchrun --standalone --nproc_per_node=1  eval.py --ref='checkpoints.zip?download=1' --outdir=outputdata --experiment_name=ambient-diffusion --seeds=0-49999 --batch=16 --corruption_probability=0.4 --delta_probability=0.1 --mask_full_rgb=True --num_masks=1 --guidance_scale=0.0 --num=50000 --with_wandb=False
